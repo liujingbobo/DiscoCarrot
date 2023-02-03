14707bd6a51b06a2a4c2710eac1f5dff052f029a
ptcheng <76898587+ptcheng@users.noreply.github.com>
2023-02-04 03:33:35 +0800

    Refactored FarmTile. added Tester to test it too

--- DiscoCarrot/Assets/Scripts/Tim/FarmTile.cs
+++ DiscoCarrot/Assets/Scripts/Tim/FarmTile.cs
@@ -6,16 +6,20 @@ using Random = UnityEngine.Random;
 
 public class FarmTile : MonoBehaviour
 {
-    public enum FarmTileState
+    public enum FarmTilePlantState
     {
         Empty,
         Plowed,
         Seeded,
         Sprouted,
+        FullyGrown,
+    }
+    public enum FarmTileEventFlag
+    {
+        NoEvent,
         NeedWater,
         NeedFertilize,
         NeedDebug,
-        FullyGrown,
     }
     
     //config values, set in editor
@@ -27,74 +31,173 @@ public class FarmTile : MonoBehaviour
     public GameObject plowedSpriteGO;
     public GameObject seededSpriteGO;
     public GameObject sproutedSpriteGO;
-    public GameObject needWaterSpriteGO;
-    public GameObject needFertilizeSpriteGO;
-    public GameObject needDebugSpriteGO;
-    public GameObject fullyGrownSpriteGO;
-    
-    
+    public List<GameObject> fullyGrownSpriteGOs; // index represent Carrot Grade
+    public List<GameObject> eventFlagSpriteGOs; // index + 1 represent FarmTileEventFlag
+
     //exposed values, dont change it
     [Header("Exposed values, dont change it")]
-    public FarmTileState currentFarmTileState;
+    public FarmTilePlantState currentPlantState;
+    public FarmTileEventFlag currentEventFlag;
+    
     public int beatCountSinceSeeded = 0;
-    public int beatCountSinceLastEvent = 0;
+    public int turnToSproutedOnBeat; //relative to beatCountSinceSeeded
+    public int turnToFullyGrownOnBeat; //relative to beatCountSinceSeeded
+    public Dictionary<int, FarmTileEventFlag> BeatsToTriggerEvent = new Dictionary<int, FarmTileEventFlag>();
 
-    //beat that trigger certian effect, set when reseting farmtile
-    public Dictionary<int, FarmTileState> beatToTriggerNeeds = new Dictionary<int, FarmTileState>();
-    public int turnToSproutedBeat;
-    public int turnToFullyGrownBeat;
+    public CarrotLevel fullyGrownCarrotLevel = CarrotLevel.Bad;
     
     private void Start()
     {
         ResetFarmTile();
         GameEvents.OnOneBeatPassed += OnOneBeatPassed;
+        GameEvents.OnFarmActionDone += OnFarmActionDone;
     }
 
+    private void OnFarmActionDone(FarmTile targetTile, PlayerFarmAction action, ActionLevel score)
+    {
+        if (targetTile != this) return;
+        switch (action)
+        {
+            case PlayerFarmAction.PlowLand:
+                SwitchPlantState(FarmTilePlantState.Plowed);
+                break;
+            case PlayerFarmAction.PlantSeed:
+                SwitchPlantState(FarmTilePlantState.Seeded);
+                break;
+            case PlayerFarmAction.WaterPlant:
+                SetFarmTileEvent(FarmTileEventFlag.NoEvent);
+                break;
+            case PlayerFarmAction.FertilizePlant:
+                SetFarmTileEvent(FarmTileEventFlag.NoEvent);
+                break;
+            case PlayerFarmAction.DebugPlant:
+                SetFarmTileEvent(FarmTileEventFlag.NoEvent);
+                break;
+            case PlayerFarmAction.HarvestPlant:
+                ResetFarmTile();
+                break;
+        }
+        
+    }
+
+    private void SwitchPlantState(FarmTilePlantState state)
+    {
+        switch (state)
+        {
+                case FarmTilePlantState.Empty:
+                    currentPlantState = FarmTilePlantState.Empty;
+                    break;
+                case FarmTilePlantState.Plowed:
+                    currentPlantState = FarmTilePlantState.Plowed;
+                    break;
+                case FarmTilePlantState.Seeded:
+                    beatCountSinceSeeded = 0;
+                    currentPlantState = FarmTilePlantState.Seeded;
+                    break;
+                case FarmTilePlantState.Sprouted:
+                    currentPlantState = FarmTilePlantState.Sprouted;
+                    break;
+                case FarmTilePlantState.FullyGrown:
+                    //pick grown type
+                    fullyGrownCarrotLevel = CalculateFinalCarrotLevel();
+                    currentPlantState = FarmTilePlantState.FullyGrown;
+                    break;
+        }
+        UpdatePlantSpriteGO(state);
+    }
+    
+    public CarrotLevel CalculateFinalCarrotLevel()
+    {
+        return CarrotLevel.Bad;
+    }
+    
+    private void UpdatePlantSpriteGO(FarmTilePlantState state)
+    {
+        switch (state)
+        {
+            case FarmTilePlantState.Empty:
+                plowedSpriteGO.SetActive(false);
+                seededSpriteGO.SetActive(false);
+                sproutedSpriteGO.SetActive(false);
+                foreach (var g in fullyGrownSpriteGOs)
+                {
+                    g.SetActive(false);
+                }
+                break;
+            case FarmTilePlantState.Plowed:
+                plowedSpriteGO.SetActive(true);
+                break;
+            case FarmTilePlantState.Seeded:
+                seededSpriteGO.SetActive(true);
+                break;
+            case FarmTilePlantState.Sprouted:
+                seededSpriteGO.SetActive(false);
+                sproutedSpriteGO.SetActive(true);
+                break;
+            case FarmTilePlantState.FullyGrown:
+                sproutedSpriteGO.SetActive(false);
+                fullyGrownSpriteGOs[(int)fullyGrownCarrotLevel].SetActive(true);
+                break;
+        }
+    }
+    private void SetFarmTileEvent(FarmTileEventFlag targetFlag)
+    {
+        switch (targetFlag)
+        {
+            case FarmTileEventFlag.NoEvent:
+                if (currentEventFlag == FarmTileEventFlag.NoEvent) return;
+                //TODO close pop up
+                //stop all sprite GO
+                foreach (var g in eventFlagSpriteGOs)
+                {
+                    g.SetActive(false);
+                }
+                break;
+            case FarmTileEventFlag.NeedWater:
+            case FarmTileEventFlag.NeedFertilize:
+            case FarmTileEventFlag.NeedDebug:
+                //TODO: trigger pop up here
+                
+                //show sprite GO
+                eventFlagSpriteGOs[(int) targetFlag - 1].SetActive(true);
+                break;
+        }
+        currentEventFlag = targetFlag;
+    }
+    
     private void OnOneBeatPassed()
     {
-        if (currentFarmTileState != FarmTileState.Empty || currentFarmTileState != FarmTileState.Plowed)
+        //add beat count since seeded
+        if (currentPlantState != FarmTilePlantState.Empty && currentPlantState != FarmTilePlantState.Plowed)
         {
             beatCountSinceSeeded += 1;
         }
-        //check if on beats that need to change state
-        if (beatCountSinceSeeded == turnToSproutedBeat && currentFarmTileState == FarmTileState.Seeded)
+        
+        //check plant state and beats to grow
+        if (currentPlantState == FarmTilePlantState.Seeded && beatCountSinceSeeded == turnToSproutedOnBeat)
         {
-            currentFarmTileState = FarmTileState.Sprouted;
+            SwitchPlantState(FarmTilePlantState.Sprouted);
         }
-        if (beatCountSinceSeeded == turnToFullyGrownBeat && currentFarmTileState == FarmTileState.Sprouted)
+        if (currentPlantState == FarmTilePlantState.Sprouted && beatCountSinceSeeded == turnToFullyGrownOnBeat)
         {
-            currentFarmTileState = FarmTileState.FullyGrown;
+            SwitchPlantState(FarmTilePlantState.FullyGrown);
         }
-
-        UpdatePlantGO();
-    }
-
-    private void UpdatePlantGO()
-    {
-        if (currentFarmTileState == FarmTileState.Seeded)
+        
+        //check if this beat will trigger event
+        if (BeatsToTriggerEvent.ContainsKey(beatCountSinceSeeded))
         {
-            seededSpriteGO.SetActive(true);
-        }
-        if (currentFarmTileState == FarmTileState.Sprouted)
-        {
-            seededSpriteGO.SetActive(false);
-            sproutedSpriteGO.SetActive(true);
-        }
-        if (currentFarmTileState == FarmTileState.FullyGrown)
-        {
-            sproutedSpriteGO.SetActive(false);
-            fullyGrownSpriteGO.SetActive(true);
+            SetFarmTileEvent(BeatsToTriggerEvent[beatCountSinceSeeded]);
         }
     }
     
     public void ResetFarmTile()
     {
-        currentFarmTileState = FarmTileState.Empty;
+        SwitchPlantState(FarmTilePlantState.Empty);
         beatCountSinceSeeded = 0;
         
-        beatToTriggerNeeds.Clear();
-        turnToSproutedBeat = 0;
-        turnToFullyGrownBeat = 0;
+        BeatsToTriggerEvent.Clear();
+        turnToSproutedOnBeat = 0;
+        turnToFullyGrownOnBeat = 0;
         SetRandomBeatTriggerValues();
     }
     
@@ -103,45 +206,55 @@ public class FarmTile : MonoBehaviour
     public void SetRandomBeatTriggerValues()
     {
         var lastestActionEndBeatLength = eventResponseTime + maxActionTime;
-        turnToSproutedBeat = seedStateNeedWaterSpawnWindow.y + lastestActionEndBeatLength;
+        turnToSproutedOnBeat = seedStateNeedWaterSpawnWindow.y + lastestActionEndBeatLength + 8;
         var seedWateringEventBeat = Random.Range(seedStateNeedWaterSpawnWindow.x, seedStateNeedWaterSpawnWindow.y);
-        beatToTriggerNeeds[seedWateringEventBeat] = FarmTileState.NeedWater;
+        BeatsToTriggerEvent[seedWateringEventBeat] = FarmTileEventFlag.NeedWater;
+        BeatsToTriggerEvent[seedWateringEventBeat+eventResponseTime] = FarmTileEventFlag.NoEvent;
         
-        var tmpEndBeat = turnToSproutedBeat;
-        List<FarmTileState> stateOptions = new List<FarmTileState>()
-            {FarmTileState.NeedWater, FarmTileState.NeedFertilize, FarmTileState.NeedDebug};
-        
+        var tmpEndBeat = turnToSproutedOnBeat;
+        List<FarmTileEventFlag> eventOptions = new List<FarmTileEventFlag>()
+            {FarmTileEventFlag.NeedWater, FarmTileEventFlag.NeedFertilize, FarmTileEventFlag.NeedDebug};
         foreach (var window in sproutStateNeedsSpawnWindow)
         {
-            tmpEndBeat = tmpEndBeat + window.y + lastestActionEndBeatLength;
-            var triggerEventBeat = Random.Range(window.x, window.y);
-            var randomEventIndex = Random.Range(0,stateOptions.Count);
-            beatToTriggerNeeds[triggerEventBeat] = stateOptions[randomEventIndex];
-            stateOptions.RemoveAt(randomEventIndex);
+            Vector2Int actualWindow = new Vector2Int(window.x + tmpEndBeat, window.y + tmpEndBeat);
+            
+            var triggerEventBeat = Random.Range(actualWindow.x, actualWindow.y);
+            var randomEventIndex = Random.Range(0,eventOptions.Count);
+            BeatsToTriggerEvent[triggerEventBeat] = eventOptions[randomEventIndex];
+            BeatsToTriggerEvent[triggerEventBeat+eventResponseTime] = FarmTileEventFlag.NoEvent;
+            eventOptions.RemoveAt(randomEventIndex);
+            
+            tmpEndBeat += window.y + lastestActionEndBeatLength + 8;
         }
 
-        turnToFullyGrownBeat = tmpEndBeat;
+        turnToFullyGrownOnBeat = tmpEndBeat;
+
+        foreach (var pair in BeatsToTriggerEvent)
+        {
+            Debug.Log($"timtest beat:{pair.Key} , event: {pair.Value}");
+        }
     }
     
     //public apis
     public PlayerFarmAction GetNeededPlayerFarmAction()
     {
-        switch (currentFarmTileState)
+        if (currentEventFlag != FarmTileEventFlag.NoEvent)
         {
-            case FarmTileState.Empty: return PlayerFarmAction.PlowLand;
-            case FarmTileState.Plowed: return PlayerFarmAction.PlantSeed;
-            case FarmTileState.Seeded: return PlayerFarmAction.NoActionNeeded;
-            case FarmTileState.Sprouted: return PlayerFarmAction.NoActionNeeded;
-            case FarmTileState.NeedWater: return PlayerFarmAction.WaterPlant;
-            case FarmTileState.NeedFertilize: return PlayerFarmAction.FertilizePlant;
-            case FarmTileState.NeedDebug: return PlayerFarmAction.DebugPlant;
-            case FarmTileState.FullyGrown: return PlayerFarmAction.HarvestPlant;
+            switch (currentEventFlag)
+            {
+                case FarmTileEventFlag.NeedWater: return PlayerFarmAction.WaterPlant;
+                case FarmTileEventFlag.NeedDebug: return PlayerFarmAction.DebugPlant;
+                case FarmTileEventFlag.NeedFertilize: return PlayerFarmAction.FertilizePlant;
+            }
         }
-        return PlayerFarmAction.PlowLand;
-    }
-
-    public CarrotLevel GetFinalCarrotLevel()
-    {
-        return CarrotLevel.Bad;
+        switch (currentPlantState)
+        {
+            case FarmTilePlantState.Empty: return PlayerFarmAction.PlowLand;
+            case FarmTilePlantState.Plowed: return PlayerFarmAction.PlantSeed;
+            case FarmTilePlantState.Seeded: return PlayerFarmAction.NoActionNeeded;
+            case FarmTilePlantState.Sprouted: return PlayerFarmAction.NoActionNeeded;
+            case FarmTilePlantState.FullyGrown: return PlayerFarmAction.HarvestPlant;
+        }
+        return PlayerFarmAction.NoActionNeeded;
     }
 }
--- DiscoCarrot/Assets/Scripts/Tim/Tester.cs
+++ DiscoCarrot/Assets/Scripts/Tim/Tester.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Tester : MonoBehaviour
 {
+    public int beatCountFromStart;
+    public FarmTile[] targetTiles;
+    public float bpm = 123;
     void Start()
     {
+        beatCountFromStart = 0;
+        StartCoroutine(AddBeat());
+        
         GameEvents.OnReachedFarmTile += OnReachedFarmTile;
         GameEvents.OnLeaveFarmTile += OnLeaveFarmTile;
     }
@@ -22,9 +28,29 @@ public class Tester : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.F))
         {
+            foreach (var targetTile in targetTiles)
+            {
+                GameEvents.OnFarmActionDone.Invoke(targetTile, PlayerFarmAction.PlowLand, ActionLevel.Perfect);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            foreach (var targetTile in targetTiles)
+            {
+                GameEvents.OnFarmActionDone.Invoke(targetTile, PlayerFarmAction.PlantSeed, ActionLevel.Perfect);
+            }
+        }
+    }
+
+    IEnumerator AddBeat()
+    {
+        while (true)
+        {
+            beatCountFromStart += 1;
             if(GameEvents.OnOneBeatPassed != null) GameEvents.OnOneBeatPassed.Invoke();
+            yield return new WaitForSeconds(  60f / bpm);
         }
     }
 }
