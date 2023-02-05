51a26f1153de5650658af6b805838b1ef0467fb8
郑倍添Tim <kentcheng105@gmail.com>
2023-02-05 14:34:21 +0800

    added tutorial stuff

--- DiscoCarrot/Assets/Scripts/UI/SongPickerUI.cs
+++ DiscoCarrot/Assets/Scripts/UI/SongPickerUI.cs
@@ -23,22 +23,26 @@ public class SongPickerUI : SerializedMonoBehaviour
 
     public void PickMagic()
     {
-        K.musicPlayer.Stop();
-        K.musicPlayer.LoadSong(MagicKoreo);
-        GameManager.singleton.stateMachine.SwitchToState(GameManager.GameLoopState.GameReadyStart);
+        //K.musicPlayer.Stop();
+
+        //K.musicPlayer.LoadSong(MagicKoreo);
+        GameManager.singleton.sharedContext.runTimeValues.pickedSongKore = MagicKoreo;
+        GameManager.singleton.stateMachine.SwitchToState(GameManager.GameLoopState.GameCutScene);
     }
 
     public void PickTLT()
     {
-        K.musicPlayer.Stop();
-        K.musicPlayer.LoadSong(TLTKoreo);
-        GameManager.singleton.stateMachine.SwitchToState(GameManager.GameLoopState.GameReadyStart);
+        //K.musicPlayer.Stop();
+        //K.musicPlayer.LoadSong(TLTKoreo);
+        GameManager.singleton.sharedContext.runTimeValues.pickedSongKore = TLTKoreo;
+        GameManager.singleton.stateMachine.SwitchToState(GameManager.GameLoopState.GameCutScene);
     }
 
     public void PickBL()
     {
-        K.musicPlayer.Stop();
-        K.musicPlayer.LoadSong(BLKoreo);
-        GameManager.singleton.stateMachine.SwitchToState(GameManager.GameLoopState.GameReadyStart);
+        //K.musicPlayer.Stop();
+        //K.musicPlayer.LoadSong(BLKoreo);
+        GameManager.singleton.sharedContext.runTimeValues.pickedSongKore = BLKoreo;
+        GameManager.singleton.stateMachine.SwitchToState(GameManager.GameLoopState.GameCutScene);
     }
 }
